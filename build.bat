e066a894d026b0c8fc5a1acf50e47071debf82bf
Benjamin Prömmer <benjamin.proemmer@outlook.com>
2017-01-10 11:36:46 +0100

    Add build info

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
+REM This script build the site under the folder docs where the ghpage is referenced
+REM To use this script you have to download docfx  https://github.com/dotnet/docfx/releases
 docfx build docfx.json

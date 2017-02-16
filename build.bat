e324b3858da387b721efee42cea59c40a7422ae1
insite-Jan <jan.haug@insite-gmbh.de>
2017-02-16 16:25:36 +0100

    added auto-update script (requires chocolatey)
    build.bat and preview.bat will now check the current version of docfx
    (set to 2.12.1.0 at the moment). If the version is not met (by
    %PROGRAMDATA%\chocolatey\bin\docfx.exe), an update script is called
    which requests admin rights and then updates docfx to the correct
    version

--- build.bat
+++ build.bat
@@ -1,3 +1,12 @@
 REM This script build the site under the folder docs where the ghpage is referenced
 REM To use this script you have to download docfx  https://github.com/dotnet/docfx/releases
+@echo off
+powershell (gi %programdata%\chocolatey\bin\docfx.exe).versioninfo.FileVersion>> "temp.txt"
+set /p DOCFXVERSION=<"temp.txt"
+del "temp.txt"
+
+IF "%DOCFXVERSION%" == "2.12.1.0" GOTO RUN
+call scripts/update_docFX.bat
+
+:RUN
 docfx build docfx.json
--- preview.bat
+++ preview.bat
@@ -1,2 +1,12 @@
+@echo off
+powershell (gi %programdata%\chocolatey\bin\docfx.exe).versioninfo.FileVersion>> "temp.txt"
+set /p DOCFXVERSION=<"temp.txt"
+del "temp.txt"
+
+IF "%DOCFXVERSION%" == "2.12.1.0" GOTO RUN
+call scripts/update_docFX.bat
+
+:RUN
 docfx build previewfx.json --serve
+
 pause
